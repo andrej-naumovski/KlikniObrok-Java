989dbf0dfbe8c23c3caac65f24bc5845865045a2
Andrej Naumovski <a.naumovski996@yahoo.com>
2016-12-08 16:44:25 +0100

    Initial project setup

---
+++ src/main/java/mk/klikniobrok/KlikniObrokApplication.java
@@ -0,0 +1,12 @@
+package mk.klikniobrok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlikniObrokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlikniObrokApplication.class, args);
+	}
+}
